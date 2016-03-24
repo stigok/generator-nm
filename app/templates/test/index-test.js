e9608c368834b2d76e4049a82b5df261cb717108
stigok <stig@stigok.com>
2016-03-24 22:22:31 +0100

    Added mocha sinon chai

---
+++ app/templates/test/index-test.js
@@ -0,0 +1,10 @@
+const assert = require('chai').assert;
+//const sinon = require('sinon');
+
+describe('<%= moduleName %>', function () {
+  it('does things', function () {
+    const things = 'good';
+    assert.notEqual(things, 'bad');
+    assert.equal(things, 'good');
+  });
+});
